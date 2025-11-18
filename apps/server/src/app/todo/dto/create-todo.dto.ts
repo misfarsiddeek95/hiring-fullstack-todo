e879ab962aae373fb4c45acdee802a6a91ed7f9b
misfarsiddeek95 <asmmisfar@gmail.com>
2025-11-18 15:28:51 +0530

    dto setup

--- apps/server/src/app/todo/dto/create-todo.dto.ts
+++ apps/server/src/app/todo/dto/create-todo.dto.ts
@@ -1,1 +1,16 @@
-export class CreateTodoDto {}
+import {
+  IsBoolean,
+  isNotEmpty,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsBoolean()
+  @IsOptional()
+  completed?: boolean;
+}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -11,7 +11,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
